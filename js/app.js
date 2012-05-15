1f52fa91507bcb1a57acd5fcf32c70298420c90b
SerjoPepper <SerjoPepper@gmail.com>
2012-05-15 14:26:25 +0400

    Конец первой части - карта и геолокация

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
 var app = {
     init: function () {
+        this.findCenter().then($.proxy(this.startGame, this));
+    },
+    
+    startGame: function (pos) {
+        this.map = new ymaps.Map("map", {
+            behaviors: ['default', 'scrollZoom'],
+            center: pos,
+            zoom: 16,
+            type: "yandex#map"
+        });
+    },
 
+    findCenter: function () {
+        var promise = new ymaps.util.Promise,
+            center = [ymaps.geolocation.latitude, ymaps.geolocation.longitude];
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (pos) {
+                promise.resolve([pos.coords.latitude, pos.coords.longitude]);
+            }, function () {
+                promise.resolve(center);
+            });
+        }
+        else {
+            setTimeout(function () { promise.resolve(center); }, 0);
+        }
+        return promise;
     }
 };
+
+ymaps.ready(function () {
+    app.init();
+});
